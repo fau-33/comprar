3f8403085fb978e776cc4a4f7581ece035f3af57
Flávio Félix <fau_felix30@hotmail.com>
2026-03-25 16:27:37 -0300

    Done components filter, input, sttatusIcon the types the home

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    paddingHorizontal: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#C3C5CB",
+    height: 48,
+    width: "100%",
+  },
+});
